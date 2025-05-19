85e3f10fb12289fb9be2eb4fb479f9f0b0e4df0c
Philipp Keysers <philipp-keysers@t-online.de>
2025-05-19 18:01:10 +0200

    Implement Navigatin Service

--- CoffeeMachine/Services/NavigationService.cs
+++ CoffeeMachine/Services/NavigationService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoffeeMachine.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,30 @@ using System.Threading.Tasks;
 
 namespace CoffeeMachine.Services
 {
-    public class NavigationService : ObservableObjec
+    public class NavigationService : ObservableObject, INavigationService
     {
+        private readonly Func<Type, ViewModel> _viewModelFactory;
+        private ViewModel _currentView;
 
+        public ViewModel CurrentView
+        {
+            get => _currentView;
+            private set
+            {
+                _currentView = value;
+                OnPropertyChanged(nameof(CurrentView));
+            }
+        }
+
+        public NavigationService(Func<Type, ViewModel> viewModelFactory)
+        {
+            _viewModelFactory = viewModelFactory;
+        }
+
+        public void NavigateTo<TViewmodel>() where TViewmodel : ViewModel
+        {
+            ViewModel viewModel = _viewModelFactory.Invoke(typeof(TViewmodel));
+            CurrentView = viewModel;
+        }
     }
 }
