dc1c237febfb3e619796ba1038683e480e924aed
Philipp Keysers <philipp-keysers@t-online.de>
2025-05-19 17:34:13 +0200

    Add Navigation Service

--- CoffeeMachine/App.xaml.cs
+++ CoffeeMachine/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Configuration;
+﻿using CoffeeMachine.MVVM.ViewModel;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +11,32 @@ namespace CoffeeMachine
     /// </summary>
     public partial class App : Application
     {
+        private readonly ServiceProvider _serviceProvider;
+
+        public App()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            services.AddSingleton<MainWindow>(provider => new MainWindow
+            {
+                DataContext = provider.GetRequiredService<MainViewModel>()
+            });
+            services.AddSingleton<MainViewModel>();
+            services.AddSingleton<HomeViewModel>();
+            services.AddSingleton<SettingsViewModel>();
+   
+
+            _serviceProvider = services.BuildServiceProvider();
+
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+            base.OnStartup(e);
+        }
+
     }
 
 }
